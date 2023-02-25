2110d6f433f356a7d37c250e0bc0a6bd3988ee96
Oleksandr Gituliar <oleksandr@gituliar.net>
2023-02-25 15:59:17 +0100

    Initial commit

---
+++ kwinto-xll.cpp
@@ -0,0 +1,12 @@
+﻿// kwinto-xll.cpp : Defines the entry point for the application.
+//
+
+#include "kwinto-xll.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
